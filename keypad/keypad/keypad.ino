ccb57abeadfaa60ba12ea45351d607b0ef30747a
Elisa Jazmin Matos Juarez <ej.matos.2020@alumnos.urjc.es>
2024-11-16 20:04:32 +0100

    añadir keypad

---
+++ keypad/keypad/keypad.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
